f9a546711745302982e7ec10b2883aba05fb442c
nbinder <nbinder@hs-heilbronn.de>
2023-05-24 10:25:21 +0200

    Added docs to signal strength

--- metrics/signal_strength.go
+++ metrics/signal_strength.go
@@ -5,13 +5,15 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-func RecordSignalStrengthEntry(receiverClientName, senderClientName string, signalStrength float64) {
-	signal.With(prometheus.Labels{receiverLabel: receiverClientName, senderLabel: senderClientName}).Set(signalStrength)
+// RecordSignalStrengthEntry
+// Records the signal strength from a sender to a receiver.
+func RecordSignalStrengthEntry(senderClientName, receiverClientName string, signalStrength float64) {
+	signal.With(prometheus.Labels{senderLabel: senderClientName, receiverLabel: receiverClientName}).Set(signalStrength)
 }
 
 var signal = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{Name: "signal_strength", Help: "Strength of the signal from receiver to sender"},
-	[]string{receiverLabel, senderLabel})
+	[]string{senderLabel, receiverLabel})
 
 const receiverLabel = "receiver"
 const senderLabel = "sender"
