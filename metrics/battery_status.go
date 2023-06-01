de09aba45f29e05bd674c647ecf588671ba994bc
soezdemir <soezdemir@stud.hs-heilbronn.de>
2023-06-01 10:43:48 +0200

    added comment

--- metrics/battery_status.go
+++ metrics/battery_status.go
@@ -5,6 +5,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+// RecordBatteryStatusEntry
+// Records the battery level of a specific client
 func RecordBatteryStatusEntry(clientName string, batteryLevel float64) {
 	batteryStatus.With(prometheus.Labels{clientNameLabel: clientName}).Set(batteryLevel)
 }
