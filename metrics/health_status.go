90e9f8b16871f90075efc0c7f0c04a6afa147764
Elias <simon.eliasv@gmail.com>
2023-06-01 10:55:15 +0200

    renamed

--- metrics/health_status.go
+++ metrics/health_status.go
@@ -8,10 +8,10 @@ import (
 // RecordHealthStatusEntry
 // Records the health status of a client.
 // uses the same senderLabel as signal_strength.go
-func RecordHealthStatusEntry(senderClientName string, healthStatus float64) {
-	signal.With(prometheus.Labels{senderLabel: senderClientName}).Set(healthStatus)
+func RecordHealthStatusEntry(clientName string, healthStatus float64) {
+	signal.With(prometheus.Labels{clientNameLabel: clientName}).Set(healthStatus)
 }
 
 var status = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{Name: "health_status", Help: "Health status of a client"},
-	[]string{senderLabel})
+	[]string{clientNameLabel})
